005aff615c2fe0000f94f9c1c58c5438c0686a37
Rrahman Rexhepi <namharr@hotmail.com>
2016-02-19 17:32:44 +0100

    Update LoginController.java

--- LoginController.java
+++ LoginController.java
@@ -5,8 +5,13 @@
  */
 package projektarbetequiz;
 
+import com.mysql.jdbc.Connection;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,10 +22,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
-
 
 public class LoginController implements Initializable {
 
@@ -28,7 +33,7 @@ public class LoginController implements Initializable {
     private TextField usernameFld;
 
     @FXML
-    private TextField passwordFld;
+    private PasswordField passwordFld;
 
     @FXML
     private Button loginBtn;
@@ -38,6 +43,15 @@ public class LoginController implements Initializable {
 
     @FXML
     private Label error;
+
+    // Static strings 
+    static String logginUsername;
+    static String tmpPassword;
+    static String tmpAccountstatus;
+
+    String dbURL = "jdbc:mysql://localhost:3306/quizdb";
+    String user = "root";
+    String pass = "XXXXX";
 
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
@@ -53,15 +67,89 @@ public class LoginController implements Initializable {
                 error.setText("Please fill in password");
 
             } else {
-// SKRIV IN DITT FXML FIL HÄR SÅ ATT DU KAN CONNECTA TILL DEN!!! 
-                
-                Parent p = FXMLLoader.load(getClass().getResource("FXMLPrincipalPanel.fxml"));
 
-                Scene s = new Scene(p);
-                Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                // Login Database 
+                try (Connection conn = (Connection) DriverManager.getConnection(dbURL, user, pass)) {
 
-                stg.setScene(s);
-                stg.show();
+                    String query = "SELECT User_name, User_password,User_status FROM useraccount WHERE user_name =?";
+                    PreparedStatement statement = (PreparedStatement) conn.prepareStatement(query);
+                    statement.setString(1, usernameFld.getText());
+                    ResultSet result = statement.executeQuery();
+                    while (result.next()) {
+
+                        // Add info to string. tmp = Temporary
+                        logginUsername = result.getString("User_name");
+                        tmpPassword = result.getString("User_password");
+                        tmpAccountstatus = result.getString("User_status");
+                        System.out.println("Connect with DataBase - Suceeded ");
+
+                    }
+
+                } catch (SQLException e) {
+                    error.setText("Can not connect to database");
+                    e.printStackTrace();
+                }
+
+                if (!usernameFld.getText().equals(logginUsername)) {
+                    // if username does not match the username user typed in
+                    error.setText("Wrong username/password");
+
+                } else if (!passwordFld.getText().equals(tmpPassword)) {
+
+                    error.setText("Wrong username/password");
+
+                } else if (tmpAccountstatus.equals("Student")) {
+
+                    usernameFld.clear();
+                    passwordFld.clear();
+
+                    Parent p = FXMLLoader.load(getClass().getResource("FXMLStudentStartMenu.fxml"));
+
+                    Scene s = new Scene(p);
+                    Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                    stg.setScene(s);
+                    stg.show();
+
+                } else if (tmpAccountstatus.equals("Teacher")) {
+
+                    usernameFld.clear();
+                    passwordFld.clear();
+
+                    Parent p = FXMLLoader.load(getClass().getResource("FXMLTeacherPanel.fxml"));
+
+                    Scene s = new Scene(p);
+                    Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                    stg.setScene(s);
+                    stg.show();
+
+                } else if (tmpAccountstatus.equals("Principal")) {
+
+                    usernameFld.clear();
+                    passwordFld.clear();
+
+                    Parent p = FXMLLoader.load(getClass().getResource("FXMLPrincipalPanel.fxml"));
+
+                    Scene s = new Scene(p);
+                    Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                    stg.setScene(s);
+                    stg.show();
+
+                } else if (tmpAccountstatus.equals("Admin")) {
+
+                    usernameFld.clear();
+                    passwordFld.clear();
+
+                    Parent p = FXMLLoader.load(getClass().getResource("FXMLAdminPanel.fxml"));
+
+                    Scene s = new Scene(p);
+                    Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                    stg.setScene(s);
+                    stg.show();
+                }
 
             }
 
