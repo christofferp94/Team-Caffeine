9b7b96aab5ee25b6b1d01a133c3bb7c38ce4ca9c
Rrahman Rexhepi <namharr@hotmail.com>
2016-02-21 15:10:18 +0100

    Update FXMLChangePasswordController.java

--- FXMLChangePasswordController.java
+++ FXMLChangePasswordController.java
@@ -5,8 +5,13 @@
  */
 package projektarbetequiz;
 
+import com.mysql.jdbc.Connection;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,8 +25,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
-
-
 
 public class FXMLChangePasswordController implements Initializable {
 
@@ -46,6 +49,10 @@ public class FXMLChangePasswordController implements Initializable {
     @FXML
     private Label error;
 
+    String dbURL = "jdbc:mysql://localhost:3306/quizdb";
+    String user = "root";
+    String pass = "XXXXX";
+
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
         if (event.getSource() == saveBtn) {
@@ -62,35 +69,63 @@ public class FXMLChangePasswordController implements Initializable {
                 message.setText("");
                 error.setText("Please confirm password");
 
+            }  else if (!currentpasswordFld.getText().equals(LoginController.tmpPassword)){
+            message.setText("");
+            error.setText("Current password does not match");
+            
             } else if (!newPasswordFld.getText().equals(confirmPasswordFld.getText())) {
                 message.setText("");
                 error.setText("Password does not match");
 
+            } else if (newPasswordFld.getText().equals(LoginController.tmpPassword)) {
+
+                message.setText("");
+                error.setText("You allready have that password!");
+
             } else {
 
+                try (Connection conn = (Connection) DriverManager.getConnection(dbURL, user, pass)) {
+
+                    String query = "UPDATE useraccount SET User_password=? WHERE User_name=?";
+                    PreparedStatement statement = (PreparedStatement) conn.prepareStatement(query);
+                    statement.setString(1, newPasswordFld.getText());
+                    statement.setString(2, LoginController.logginUsername);
+                    statement.executeUpdate();
+
+                } catch (SQLException e) {
+
+                    System.out.println("Can not connect to database");
+                    error.setText("Can not connect to database");
+                    e.printStackTrace();
+                }
+
                 error.setText("");
-                message.setText("Password changed");
+                message.setText("Changed password!");
+
+                LoginController.tmpPassword = newPasswordFld.getText();
+                currentpasswordFld.clear();
+                newPasswordFld.clear();
+                confirmPasswordFld.clear();
 
             }
 
         } else if (event.getSource() == backBtn) {
             // When user clicks Back button 
-            
-             Parent p = FXMLLoader.load(getClass().getResource("FXMLStudentStartMenu.fxml"));
+
+            Parent p = FXMLLoader.load(getClass().getResource("FXMLStudentStartMenu.fxml"));
 
             Scene s = new Scene(p);
             Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
             stg.setScene(s);
             stg.show();
-            
-            
+
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }
 
 }
